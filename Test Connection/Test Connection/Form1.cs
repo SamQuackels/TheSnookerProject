ef0d68219ac6af76737608c290fb6ce225d78b5b
SamQuackels <95223452+SamQuackels@users.noreply.github.com>
2021-12-06 17:54:12 +0100

    Dropbox Tests
    Dropbox kan alle groupmatches weergeven en als je dan op button2 duwt laat het alle frames zien van die gekozen groupmatch

--- Test Connection/Test Connection/Form1.cs
+++ Test Connection/Test Connection/Form1.cs
@@ -13,6 +13,8 @@ namespace Test_Connection
 {
     public partial class Form1 : Form
     {
+        // String aanmaken zodat je kan verbinden met de server
+        string ConnectString = "datasource = localhost; port = 3306; username = root; password=; database = snooker";
         public Form1()
         {
             InitializeComponent();
@@ -25,14 +27,12 @@ namespace Test_Connection
 
         private void DBConnection()
         {
-            string s;
-
-            // String aanmaken zodat je kan verbinden met de server
-            string ConnectString = "datasource = localhost; port = 3306; username = root; password=; database = snooker";
             if (tryConnection(ConnectString))
             {
                 MessageBox.Show("Verbinding Succesvol");
             }
+
+            AddItems(ConnectString, listBox1);
 
             // SQL Query om alle users met ID 2 hun naam aan te passen
             string updateName =
@@ -54,89 +54,115 @@ namespace Test_Connection
             //string highestPKID =
             //    @"SELECT max(ID) FROM groupmatches";
 
-
-
-
             // Voert SQL Querys uit
             //ExecuteCommand(ConnectString, addUser);
             //ExecuteCommand(ConnectString, updateName);
             //ExecuteCommand(ConnectString, deleteUser);        
             //string s = GetData(ConnectString, writeNames);
             //string a = GetData(ConnectString, highestPKID);
-            getGroupMatchData(3);
+            //getGroupMatchData(3, ConnectString);
+        }
 
-            void ExecuteCommand(string connectionString, string commandString)
+        void getGroupMatchData(int ID, string ConnectString)
+        {
+            string command = String.Format(@"SELECT ID FROM matches WHERE GroupMatchID = '{0}'", ID);
+            string ids = GetData(ConnectString, command);
+            command = String.Format(@"SELECT * FROM frames WHERE MatchID = '{0}' OR MatchID = '{1}' OR MatchID = '{2}'", ids[0], ids[1], ids[2]);
+            string fids = GetData(ConnectString, command);
+            MessageBox.Show(fids);
+        }
+
+        void ExecuteCommand(string connectionString, string commandString)
+        {
+            // Controleert of er een verbinding is
+            if (tryConnection(connectionString))
             {
-                // Controleert of er een verbinding is
-                if (tryConnection(connectionString))
+                // Met gebruik van de connectie
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    // Met gebruik van de connectie
-                    using (MySqlConnection connection = new MySqlConnection(connectionString))
-                    {
-                        // Command aanmaken
-                        MySqlCommand command = new MySqlCommand(commandString, connection);
-                        // Connectie openen
-                        connection.Open();
-                        // Commando doorsturen
-                        command.ExecuteNonQuery();
-                    }
+                    // Command aanmaken
+                    MySqlCommand command = new MySqlCommand(commandString, connection);
+                    // Connectie openen
+                    connection.Open();
+                    // Commando doorsturen
+                    command.ExecuteNonQuery();
                 }
             }
-
-            string GetData(string connectionString, string commandString)
+        }
+        bool tryConnection(string connectionString)
+        {
+            bool connected = true;
+            MySqlConnection DBConnect = new MySqlConnection(connectionString);
+            try
             {
-                string str = "";
-                // Controleert of er een verbinding is
-                if (tryConnection(connectionString))
+                DBConnect.Open();
+            }
+            catch (Exception ex)
+            {
+                connected = false;
+                MessageBox.Show(ex.Message);
+            }
+            return connected;
+        }
+
+
+        string GetData(string connectionString, string commandString)
+        {
+            string str = "";
+            // Controleert of er een verbinding is
+            if (tryConnection(connectionString))
+            {
+                // Met gebruik van de connectie
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    // Met gebruik van de connectie
-                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    // Commando aanmaken
+                    MySqlCommand command = new MySqlCommand(commandString, connection);
+                    // Connectie openen
+                    connection.Open();
+                    // Met gebruik van de "DataReader"
+                    using (MySqlDataReader reader = command.ExecuteReader())
                     {
-                        // Commando aanmaken
-                        MySqlCommand command = new MySqlCommand(commandString, connection);
-                        // Connectie openen
-                        connection.Open();
-                        // Met gebruik van de "DataReader"
-                        using (MySqlDataReader reader = command.ExecuteReader())
+                        // Terwijl het verschillende objecten leest 
+                        while (reader.Read())
                         {
-                            // Terwijl het verschillende objecten leest 
-                            while (reader.Read())
-                            {
-                                // Laat elk individueel object zien in een MessageBox
-                                //MessageBox.Show(String.Format(Convert.ToString(reader[0])));
-                                str += String.Format(Convert.ToString(reader[0]));
-                            }
+                            // Laat elk individueel object zien in een MessageBox
+                            //MessageBox.Show(String.Format(Convert.ToString(reader[0])));
+                            str += String.Format(Convert.ToString(reader[0]));
                         }
                     }
                 }
-                return str;
             }
+            return str;
+        }
 
-            // Controleert of er connectie is
-            bool tryConnection(string connectionString)
+        void AddItems(string connectionString, ListBox lb)
+        {
+            string commandString = @"SELECT id FROM groupmatches";
+            if (tryConnection(connectionString))
             {
-                bool connected = true;
-                MySqlConnection DBConnect = new MySqlConnection(connectionString);
-                try
+                // Met gebruik van de connectie
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    DBConnect.Open();
+                    // Commando aanmaken
+                    MySqlCommand command = new MySqlCommand(commandString, connection);
+                    // Connectie openen
+                    connection.Open();
+                    // Met gebruik van de "DataReader"
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Terwijl het verschillende objecten leest 
+                        while (reader.Read())
+                        {
+                            lb.Items.Add(Convert.ToString(reader[0]));
+                        }
+                    }
                 }
-                catch (Exception ex)
-                {
-                    connected = false;
-                    MessageBox.Show(ex.Message);
-                }
-                return connected;
             }
+        }
 
-            void getGroupMatchData(int ID)
-            {
-                string command = String.Format(@"SELECT ID FROM matches WHERE GroupMatchID = '{0}'", ID);
-                string ids = GetData(ConnectString, command);
-                command = String.Format(@"SELECT * FROM frames WHERE MatchID = '{0}' OR MatchID = '{1}' OR MatchID = '{2}'", ids[0], ids[1], ids[2]);
-                string fids = GetData(ConnectString, command);
-                MessageBox.Show(fids);
-            }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getGroupMatchData(Convert.ToInt32(listBox1.SelectedItem) ,ConnectString); 
         }
     }
 }
